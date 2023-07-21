48be61f3d2ac9d0ecd69f9d22bf22c67f8746866
CuteLittleSky <62592731+CuteLittleSky@users.noreply.github.com>
2023-07-21 10:46:57 +0800

    Update Generators.java

--- src/main/java/io/github/mooy1/infinityexpansion/items/generators/Generators.java
+++ src/main/java/io/github/mooy1/infinityexpansion/items/generators/Generators.java
@@ -20,14 +20,14 @@ public final class Generators {
 
     private static final int HYDRO_ENERGY = 5;
     private static final int ADVANCED_HYDRO_ENERGY = 45;
-    private static final int GEO_ENERGY = 35;
-    private static final int ADVANCED_GEO_ENERGY = 210;
-    private static final int BASIC_SOLAR_ENERGY = 9;
-    private static final int ADVANCED_SOLAR_ENERGY = 150;
-    private static final int CELESTIAL_ENERGY = 750;
-    private static final int VOID_ENERGY = 3000;
-    private static final int INFINITY_ENERGY = 60_000;
-    private static final int INFINITY_REACTOR_ENERGY = 120_000;
+    private static final int GEO_ENERGY = 25;
+    private static final int ADVANCED_GEO_ENERGY = 150;
+    private static final int BASIC_SOLAR_ENERGY = 6;
+    private static final int ADVANCED_SOLAR_ENERGY = 90;
+    private static final int CELESTIAL_ENERGY = 450;
+    private static final int VOID_ENERGY = 1800;
+    private static final int INFINITY_ENERGY = 36_000;
+    private static final int INFINITY_REACTOR_ENERGY = 72_000;
 
     public static final SlimefunItemStack INFINITY_REACTOR = new SlimefunItemStack(
             "INFINITY_REACTOR",
